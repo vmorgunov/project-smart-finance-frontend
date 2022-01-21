f1bf64e111780f55ede934a6caa5ca40bc306731
Kseniia <76482697+Kseniia-FS@users.noreply.github.com>
2022-01-21 16:10:50 +0200

    Added token function

--- src/api/reportApi.js
+++ src/api/reportApi.js
@@ -3,9 +3,20 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://project-smart-finance.herokuapp.com/api/v1/';
 
+export const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = "";
+  },
+};
+
+
 export async function getAllTransactionsDATA({ YYYY, MM, type }) {
-  const data = await axios.get(
+  const {data} = await axios.get(
     `/transactions/:year=${YYYY}/:month=${MM}/:type=${type}/data`,
   );
+  token.set(data.token);
   return data;
 }
