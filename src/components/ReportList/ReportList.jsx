85138c348491a3ca662e86208b0460d09950deca
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2022-01-22 12:41:07 +0100

    bugfix section

--- src/components/ReportList/ReportList.jsx
+++ src/components/ReportList/ReportList.jsx
@@ -13,7 +13,7 @@ const ReportList = ({ category, transactions }) => {
             {transactions.length === 0 ? (<Item>Транзакций нет</Item>) :
             
                 (transactions.map(item =>
-                (<Item>
+                (<Item key={item.id}>
                     <p>{item.sum}</p>
                     <img width="58" height="58" src={repAlcohol} alt="repAlcohol" />
                     <p>{item.category}</p>
--- src/components/ReportStatistic/ReportStatistic.jsx
+++ src/components/ReportStatistic/ReportStatistic.jsx
@@ -16,7 +16,7 @@ import {
   Line
 } from './ReportStatistic.styled';
 
-export const ReportStatistic = () => {
+export const ReportStatistic = ({costs, income}) => {
 
   const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 767 });
   const isTabletOrDesktop = useMediaQuery({ minWidth: 768 }); 
@@ -26,16 +26,16 @@ export const ReportStatistic = () => {
        
        {isMobile && 
          <ContainerMobile>
-         <Title>Расходы:{<SpanRedMobile>-{} грн</SpanRedMobile>} </Title>
+         <Title>Расходы:{<SpanRedMobile>-{costs} грн</SpanRedMobile>} </Title>
              <LineMobile />
-         <Title>Доходы:{<SpanMobile>+ {} грн</SpanMobile>} </Title>
+         <Title>Доходы:{<SpanMobile>+ {income} грн</SpanMobile>} </Title>
          </ContainerMobile>}
        
        {isTabletOrDesktop && 
          <ContainerDesktop>
-         <Title>Расходы:{<SpanRed>-{} грн</SpanRed>} </Title>
+         <Title>Расходы:{<SpanRed>-{costs} грн</SpanRed>} </Title>
             <Line />
-        <Title>Доходы:{<Span>+ {} грн</Span>} </Title>
+        <Title>Доходы:{<Span>+ {income} грн</Span>} </Title>
         </ContainerDesktop> 
         }   
     </Container>   
