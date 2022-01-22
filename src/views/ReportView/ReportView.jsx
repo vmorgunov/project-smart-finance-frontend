69bf44b8ffe2741ddeb5fcb637068463105fb11c
Vitalii Morgunov <utlab13@gmail.com>
2022-01-22 19:50:37 +0100

    Fix bugs in report view

--- src/views/ReportView/ReportView.jsx
+++ src/views/ReportView/ReportView.jsx
@@ -25,65 +25,6 @@ import {
 } from './ReportView.styled';
 import { useDispatch } from 'react-redux';
 
-const transactions = [
-  {
-    _id: '61e878e438630fdee30eaf62',
-    type: 'costs',
-    category: 'спорт,хобби',
-    description: 'gym',
-    sum: 350,
-    day: '09',
-    month: '12',
-    year: '2021',
-    owner: '61e8754f36895bfe4aef28fc',
-    createdAt: '2022-01-19T20:47:32.639Z',
-    updatedAt: '2022-01-19T20:47:32.639Z',
-    date: '09.12.2021',
-  },
-  {
-    _id: '61e8791438630fdee30eaf67',
-    type: 'costs',
-    category: 'прочее',
-    description: 'killer',
-    sum: 10000,
-    day: '09',
-    month: '12',
-    year: '2021',
-    owner: '61e8754f36895bfe4aef28fc',
-    createdAt: '2022-01-19T20:48:20.311Z',
-    updatedAt: '2022-01-19T20:48:20.311Z',
-    date: '09.12.2021',
-  },
-  {
-    _id: '61e8794e38630fdee30eaf6c',
-    type: 'costs',
-    category: 'коммуналка, связь',
-    description: 'water',
-    sum: 500,
-    day: '09',
-    month: '12',
-    year: '2021',
-    owner: '61e8754f36895bfe4aef28fc',
-    createdAt: '2022-01-19T20:49:18.125Z',
-    updatedAt: '2022-01-19T20:49:18.125Z',
-    date: '09.12.2021',
-  },
-  {
-    _id: '61e8795638630fdee30eaf71',
-    type: 'costs',
-    category: 'коммуналка, связь',
-    description: 'water',
-    sum: 512,
-    day: '09',
-    month: '12',
-    year: '2021',
-    owner: '61e8754f36895bfe4aef28fc',
-    createdAt: '2022-01-19T20:49:26.964Z',
-    updatedAt: '2022-01-19T20:49:26.964Z',
-    date: '09.12.2021',
-  },
-];
-
 const ReportView = () => {
   const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 767 });
   const isTabletOrDesktop = useMediaQuery({ minWidth: 768 });
@@ -98,80 +39,37 @@ const ReportView = () => {
   const userToken = useSelector(getUserToken);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (!!userToken) {
+      const transactionsData = dispatch(
+        getTransactionsPreMonthForChart({ year, month, switchData, userToken }),
+      );
+      setData(transactionsData);
+    }
+  }, [year, dispatch, month, userToken, switchData]);
 
-// Расход и доход
+  //   const switchMonthLeft = () => {
+  //     setMonth(newDate.add(-1, 'month').format('MM'));
+  //     if (month === '01') {
+  //       setYear(newDate.add('year').format('YYYY'));
+  //     }
+  //   };
 
-    const switchMonthLeft = () => {
-        setDateMonth(newDate.add(-1, 'month').format('MM'));
-        if (dateMonth === '01') {
-            setDateYears(newDate.add('year').format('YYYY'));
-        }
-    };
+  //   const switchMonthRight = () => {
+  //     setMonth(newDate.add(1, 'month').format('MM'));
+  //     if (month === '12') {
+  //       setYear(newDate.add('year').format('YYYY'));
+  //     }
+  //   };
 
-    const switchMonthRight = () => {
-        setDateMonth(newDate.add(1, 'month').format('MM'));
-        if (dateMonth === '12') {
-            setDateYears(newDate.add('year').format('YYYY'));
-        }
-    };
-
-    const clickOnSwitch = () => {
-        if (switchData === 'costs') {
-            setSwitchData('income');
-        }
-        if (switchData !== 'costs') {
-            setSwitchData('costs');
-        }
-    };
-
-    return (
-        <>
-            <ReportContainer>
-                {isMobile &&
-                    <>
-                        <ReportButton />
-
-                        <MonthPicker
-                            switchMonthLeft={switchMonthLeft}
-                            switchMonthRight={switchMonthRight}
-                            dateMonth={dateMonth}
-                            dateYears={dateYears}
-                        />
-                    </>
-                }
-                {isTabletOrDesktop &&
-                    <ReportHeader>
-
-                        <ReportButton />
-
-                        {/* <Balance/> */}
-
-                        <MonthPicker
-                            switchMonthLeft={switchMonthLeft}
-                            switchMonthRight={switchMonthRight}
-                            dateMonth={dateMonth}
-                            dateYears={dateYears}
-                        />
-
-                    </ReportHeader>
-                }
-                 {/* {isTabletOrDesktop && <ModalOut />} */}
-                <ReportStatistic>
-                    costs={categoriesCosts}
-                    income={categoriesIncome}
-                </ReportStatistic>
-
-                <Reports
-                    allCategories={allCategories}
-                    switchData={switchData}
-                    clickOnSwitch={clickOnSwitch}
-                />
-
-                <ReportGraph>29. График</ReportGraph>
-                <Chart />
-            </ReportContainer>
-        </>
-    );
+  //   const clickOnSwitch = () => {
+  //     if (switchData === 'costs') {
+  //       setSwitchData('income');
+  //     }
+  //     if (switchData !== 'costs') {
+  //       setSwitchData('costs');
+  //     }
+  //   };
 
   return (
     <>
