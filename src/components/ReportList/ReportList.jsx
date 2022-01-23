f7f55ce68893cd7fb2262157a54052d7eec65e9f
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-01-23 22:20:52 +0100

    addSprite-style-icons

--- src/components/ReportList/ReportList.jsx
+++ src/components/ReportList/ReportList.jsx
@@ -1,35 +1,49 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { getIsFulfilled } from '../../redux/transactonsForChart/chartSelectors';
 
-import {List, Item, ItemData, ItemImg} from './ReportList.styled'
-import repAlcohol from '../../images/rep-alcohol.svg'
-// import salary from '../../images/salary.svg'
+import {
+  List,
+  Item,
+  ItemValue,
+  ItemTitle,
+  SvgBox,
+  Svg,
+} from './ReportList.styled';
+
+import icons from '../../images/symbol-defs.svg';
 
 const ReportList = ({ data, onClickGetChart }) => {
-    const isFulfilled = useSelector(getIsFulfilled);
-    const [isActiveIdx, setIsActiveId] = useState('');
+  const isFulfilled = useSelector(getIsFulfilled);
+  const [isActiveIdx, setIsActiveId] = useState('');
 
-    return (
-        <List>
-            {!isFulfilled ? ( <Item>-L-O-A-D-I-N-G-</Item> )
-                :
-            (data.length === 0 ? ( <Item>Транзакций нет</Item>)
-                :
-            (data.map((item, index) =>
-                (<Item key={index}
-                    onClick={() => {
-                        onClickGetChart(index);
-                        setIsActiveId(index);
-                    }} >
-                    <ItemData>{item.sum}</ItemData>
-                    <ItemImg width="58" height="58" src={repAlcohol} alt="repAlcohol" />
-                    <ItemData>{item.category}</ItemData>
-                </Item>)))
-                )
-            }
-        </List>
-    )
-}
+  return (
+    <List>
+      {!isFulfilled ? (
+        <Item>-L-O-A-D-I-N-G-</Item>
+      ) : data.length === 0 ? (
+        <Item>Транзакций нет</Item>
+      ) : (
+        data.map((item, index) => (
+          <Item
+            key={index}
+            onClick={() => {
+              onClickGetChart(index);
+              setIsActiveId(index);
+            }}
+          >
+            <ItemValue>{item.sum}</ItemValue>
+            <SvgBox>
+              <Svg width="58" height="58">
+                <use xlinkHref={`${icons}#${item.category}`} />
+              </Svg>
+            </SvgBox>
+            <ItemTitle>{item.category}</ItemTitle>
+          </Item>
+        ))
+      )}
+    </List>
+  );
+};
 
 export default ReportList;
