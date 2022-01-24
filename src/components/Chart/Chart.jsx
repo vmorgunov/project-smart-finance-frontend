9da67ce0c6d8251216b1a96f389301cd99d3b0cd
Vitalii Morgunov <utlab13@gmail.com>
2022-01-24 22:01:20 +0100

    Prettier fix

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -18,66 +18,58 @@ ChartJS.register(
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 );
 
-                    
-export function Chart ({ transactions, categories, chartsCategoryId}) {
+export function Chart({ transactions, categories, chartsCategoryId }) {
   const isTabletOrDesktop = useMediaQuery({ minWidth: 768 });
-  const isMobile = useMediaQuery({minWidth: 320, maxWidth: 767 })
+  const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 767 });
   const optionsMobile = {
     indexAxis: 'y',
-      elements: {
-    bar: {
-      borderWidth: 1,
+    elements: {
+      bar: {
+        borderWidth: 1,
+      },
     },
-  },
-   responsive: true,
-   maintainAspectRatio : false,
-  plugins: {
-    legend: {
-      position: 'top',
-      display: false,
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: 'top',
+        display: false,
+      },
     },
-  },
   };
-  
+
   const optionsDesktop = {
-   responsive: true,
-   plugins: {
-    legend: {
-       position: 'top',
-       display: false,
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+        display: false,
+      },
+      title: {
+        display: false,
+        text: 'График',
+      },
     },
-    title: {
-      display: false,
-      text: 'График',
-    },
-  },
-};
+  };
 
   const data = {
-    labels: [ chartsCategoryId? transactions: categories ],
+    labels: [chartsCategoryId ? transactions : categories],
     datasets: [
       {
         label: ['1', '2', '3'],
         data: [1, 2, 3],
         backgroundColor: 'rgba(255, 117, 29, 1)',
-    },
-  ],
-};
-  
- return (
-   <Container>
-     {isMobile &&
-       <Bar options={optionsMobile} data={data} />
-       }
-     {isTabletOrDesktop &&
-       <Bar options={optionsDesktop} data={data} />
-     }
-   </Container >
-  );
-};
-  
-    
+      },
+    ],
+  };
 
+  return (
+    <Container>
+      {isMobile && <Bar options={optionsMobile} data={data} />}
+      {isTabletOrDesktop && <Bar options={optionsDesktop} data={data} />}
+    </Container>
+  );
+}
