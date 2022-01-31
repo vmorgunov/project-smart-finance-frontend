6ec4dfbb20180f6a6f9ca6415b375cf7f90259ef
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-01-31 20:34:04 +0100

    MaterialUISwitch + index.css[data-theme] + appBar+use-theme

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
 import { getIsLoggedIn } from '../../redux/auth/authSelectors';
 import UserMenu from '../UserMenu';
 
+import { useTheme } from '../../common/hooks/use-theme';
+import MaterialUISwitch from 'components/ThemeSwitcher/ThemeSwitcher';
 import logo from '../../images/logo.svg';
 import { Container, Logo } from './AppBar.styled.jsx';
 
@@ -14,12 +16,31 @@ const AppBar = () => {
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
   const isMobile = useMediaQuery({ maxWidth: 767 });
   const matches = { isDesktop, isTablet, isMobile };
+  // MaterialUISwitch
+  const { theme, setTheme } = useTheme();
+  const [checked, setChecked] = useState(true);
+  // MaterialUISwitch
+  useEffect(() => {
+    const isChecked = localStorage.getItem('theme');
+    isChecked === 'light' ? setChecked(false) : setChecked(true);
+  }, []);
+  // MaterialUISwitch
+  const handleChange = event => {
+    setChecked(event.target.checked);
+    !checked === true ? setTheme('dark') : setTheme('light');
+    console.log(event.target.checked);
+  };
 
   return (
     <Container matches={matches}>
       <Logo href="/">
         <img src={logo} alt="Логотип" />
       </Logo>
+      <MaterialUISwitch
+        checked={checked}
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'controlled' }}
+      />
       {isLoggedIn && <UserMenu />}
     </Container>
   );
